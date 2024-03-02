2483fc98b53903aa9b0271514bba536f57882dd7
Pereira2002 <josepablopereira2002@gmail.com>
2024-03-01 22:43:27 -0600

    inicio

--- src/index.js
+++ src/index.js
@@ -3,15 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { Cuerpo } from './components';
-import Footer from './components/Footer'; 
-import { NavBar } from './components/NavBar'; 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-      <NavBar/> 
-      <Cuerpo/>
-      <Footer/>
+      <App/>
   </React.StrictMode>
 );
 
